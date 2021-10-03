5f0d5bcf7f6d15d5efb2db413912c0f54db23edd
Ai Kurashina <okamoto.ai1@gmail.com>
2021-10-03 16:44:29 -0400

    test

--- videoproj/contactpage/forms.py
+++ videoproj/contactpage/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.forms import ModelForm
+from .models import Contact
 
 
-class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
